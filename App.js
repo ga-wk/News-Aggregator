7fab199b9d02d010cdc53c42d20a67eaa0f3e993
Andrey <agazzaev88@gmail.com>
2021-04-06 17:51:04 +0700

    work at NabBar

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { Index } from "./src/index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Android 🎉</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Index />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
