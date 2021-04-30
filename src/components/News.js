56ad4207d942bc8e2cfcf73e8fa59c52a3313fc0
Andrey <agazzaev88@gmail.com>
2021-04-30 18:11:15 +0700

    подгрузка новостей

--- src/components/News.js
+++ src/components/News.js
@@ -6,8 +6,8 @@ import {
   Button,
   FlatList,
   TextInput,
-  Image,
 } from "react-native";
+
 import { Card } from "react-native-elements";
 
 const Item = ({ title }) => (
@@ -16,22 +16,22 @@ const Item = ({ title }) => (
   </View>
 );
 
-const NewsItem = ({ title, urlToImage, description }) => (
-
+const NewsItem = ({ title, urlToImage, description, index }) => (
   <Card>
-    <Card.Title>{title}</Card.Title>
-    <Card.Divider/>
-    <Card.Image  source={{uri:urlToImage}}/>
-    <Card.Divider/>
+    <Card.Title>{index + title}</Card.Title>
+    <Card.Divider />
+    <Card.Image source={{ uri: urlToImage }} />
+    <Card.Divider />
     <Text style={{ marginBottom: 10 }}>{description}</Text>
   </Card>
 );
 
-const renderNewsItem = ({ item }) => (
+const renderNewsItem = ({ item, index }) => (
   <NewsItem
     title={item["title"]}
     urlToImage={item["urlToImage"]}
     description={item["description"]}
+    index={index}
   />
 );
 const renderItem = ({ item }) => <Item title={item.title} />;
@@ -41,8 +41,18 @@ export const News = (props) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [news, setNews] = useState(null);
 
+  let page = 1;
   const url =
-    "https://newsapi.org/v2/top-headlines?country=ru&pageSize=10&apiKey=1c0f29e3b4504430822fb18fd64d6988";
+    "https://newsapi.org/v2/everything?" +
+    "domains=tvrain.ru&" +
+    "page=" +
+    page +
+    "&" +
+    "pageSize=" +
+    10 +
+    "&" +
+    "language=ru&" +
+    "apiKey=1c0f29e3b4504430822fb18fd64d6988";
   const req = new Request(url);
 
   useEffect(() => {
@@ -88,11 +98,36 @@ export const News = (props) => {
         <Button title={"Категории"} onPress={categoriesClick} />
       </View>
       <View style={styles.content}>
+        {isLoaded && news && console.log(news)}
         {isLoaded && news ? (
           <FlatList
             data={news["articles"]}
             renderItem={renderNewsItem}
-            keyExtractor={(item) => item["title"]}
+            keyExtractor={(item, index) => index.toString()}
+            onEndReached={() => {
+              ++page;
+              const url =
+                "https://newsapi.org/v2/everything?" +
+                "domains=tvrain.ru&" +
+                "page=" +
+                page +
+                "&" +
+                "pageSize=" +
+                10 +
+                "&" +
+                "language=ru&" +
+                "apiKey=1c0f29e3b4504430822fb18fd64d6988";
+              console.log(page);
+              const req = new Request(url);
+
+              fetch(req)
+                .then((res) => res.json())
+                .then((data) => {
+                  let tmpArr = news;
+                  tmpArr["articles"].push(...data["articles"]);
+                  setNews(tmpArr);
+                });
+            }}
           />
         ) : (
           <Text>Загрузка</Text>
