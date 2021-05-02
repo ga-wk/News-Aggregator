09e8455fa6b383bf87b9990bcf4196c05cb7da40
Andrey <agazzaev88@gmail.com>
2021-05-02 20:49:07 +0700

    фильтр по дате

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,6 +7,7 @@ import {
   FlatList,
   TextInput,
   ActivityIndicator,
+  Linking,
 } from "react-native";
 import { Card } from "react-native-elements";
 import {
@@ -18,7 +19,7 @@ import {
   all,
   btnCategories,
 } from "../strings/stringNew";
-import { key , allSources } from "../strings/public";
+import { key, allSources } from "../strings/public";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 export const News = (props) => {
@@ -26,20 +27,22 @@ export const News = (props) => {
   const [categoriesVisible, setCategoriesVisible] = useState(false);
   const newsList = useRef(null);
 
+  //пока так
+  const pageFirst = 1;
+  const size = 10;
+  const country = "ru";
+
   //news
   const [isLoaded, setIsLoaded] = useState(false);
   const [news, setNews] = useState(false);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(pageFirst);
   const [curCategory, setCurCategory] = useState(all);
   const [curEndpoint, setCurEndpoint] = useState(everything);
-
-  //пока так
-  const size = 10;
-  const country = "ru";
 
   // date
   const [fromDate, setFromDate] = useState(new Date());
   const [toDate, setToDate] = useState(new Date());
+  const [curDate, setCurDate] = useState("");
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
 
@@ -48,9 +51,16 @@ export const News = (props) => {
   const [input, setInput] = useState(undefined);
 
   const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
+    const currentDate = selectedDate || new Date();
     setShow(Platform.OS === "ios");
-    setDate(currentDate);
+    switch (curDate) {
+      case "from":
+        setFromDate(currentDate);
+        break;
+      case "to":
+        setToDate(currentDate);
+        break;
+    }
   };
 
   const showMode = (currentMode) => {
@@ -58,7 +68,15 @@ export const News = (props) => {
     setMode(currentMode);
   };
 
-  const showDatepicker = () => {
+  const showDatepicker = (date) => {
+    switch (date) {
+      case "from":
+        setCurDate("from");
+        break;
+      case "to":
+        setCurDate("to");
+        break;
+    }
     showMode("date");
   };
 
@@ -74,14 +92,18 @@ export const News = (props) => {
     country,
     apiKey,
   }) => {
+    tmpfrom = from.toLocaleDateString().split("/");
+    tmpfrom = `20${tmpfrom[2]}-${tmpfrom[0]}-${tmpfrom[1]}`;
+    tmpto = to.toLocaleDateString().split("/");
+    tmpto = `20${tmpto[2]}-${tmpto[0]}-${tmpto[1]}`;
+
+    console.log("from", from, "to", to);
     switch (endpoint) {
       case everything:
-        console.log("qqqqqqqqqqqqqqq", q);
         if (q) {
-          console.log("qqqqqqqqqqqqqqq", q);
-          return `https://newsapi.org/v2/${everything}?q=${q}&from=${from}to=${to}&&domains=${domains}&page=${page}&pageSize=${pageSize}&language=${country}&apiKey=${apiKey}`;
+          return `https://newsapi.org/v2/${everything}?q=${q}&from=${tmpfrom}to=${tmpto}&domains=${domains}&page=${page}&pageSize=${pageSize}&language=${country}&apiKey=${apiKey}`;
         }
-        return `https://newsapi.org/v2/${everything}?domains=${domains}&page=${page}&pageSize=${pageSize}&language=${country}&apiKey=${apiKey}`;
+        return `https://newsapi.org/v2/${everything}?q=&from=${from.toUTCString()}to=${to.toUTCString()}&domains=${domains}&page=${page}&pageSize=${pageSize}&language=${country}&apiKey=${apiKey}`;
       case topHeadlines:
         return `https://newsapi.org/v2/${topHeadlines}?category=${category}&page=${page}&pageSize=${pageSize}&country=${country}&apiKey=${apiKey}`;
       case sources:
@@ -106,13 +128,41 @@ export const News = (props) => {
     </View>
   );
 
-  const NewsItem = ({ title, urlToImage, description }) => (
+  const OpenURLButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      // Checking if the link is supported for links with custom URL scheme.
+      const supported = await Linking.canOpenURL(url);
+
+      if (supported) {
+        // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+        // by some browser in the mobile
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Don't know how to open this URL: ${url}`);
+      }
+    }, [url]);
+
+    return <Button title={children} onPress={handlePress} />;
+  };
+
+  const NewsItem = ({ title, urlToImage, description, url, publishedAt }) => (
     <Card>
       <Card.Title>{title}</Card.Title>
       <Card.Divider />
       <Card.Image source={{ uri: urlToImage }} />
       <Card.Divider />
       <Text style={{ marginBottom: 10 }}>{description}</Text>
+      <Card.Divider />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <OpenURLButton url={url}>Открыть</OpenURLButton>
+        <Text>{publishedAt}</Text>
+      </View>
     </Card>
   );
 
@@ -121,6 +171,8 @@ export const News = (props) => {
       title={item["title"]}
       urlToImage={item["urlToImage"]}
       description={item["description"]}
+      url={item["url"]}
+      publishedAt={item["publishedAt"]}
     />
   );
 
@@ -152,7 +204,6 @@ export const News = (props) => {
       country,
       apiKey
     );
-
     const promisNews = createPromis(url);
     const res = await promisNews;
     const data = await res.json();
@@ -163,26 +214,31 @@ export const News = (props) => {
   const setNewCategory = (newCategory) => {
     categoriesClick();
     setCurCategory(newCategory);
-    setPage(1);
-    const size = 10;
-    const country = "ru";
-    const page = 1;
+    setPage(pageFirst);
+    setInput(undefined);
+
     if (newCategory === "all") {
       getNews({
         endpoint: everything,
         category: curCategory,
         domains: allSources,
-        page: page,
+        from: new Date(),
+        to: new Date(),
+        page: pageFirst,
         pageSize: size,
         country: country,
         apiKey: key,
       }).then((data) => setNews(data));
       setCurEndpoint(everything);
+      setFromDate(new Date());
+      setToDate(new Date());
     } else {
       getNews({
         endpoint: topHeadlines,
         category: newCategory,
-        page: page,
+        from: fromDate,
+        to: toDate,
+        page: pageFirst,
         pageSize: size,
         country: country,
         apiKey: key,
@@ -192,8 +248,8 @@ export const News = (props) => {
   };
 
   const searching = () => {
-    setCurCategory("all");
-    setPage(1);
+    setCurCategory(all);
+    setPage(pageFirst);
     getNews({
       endpoint: everything,
       category: curCategory,
@@ -205,18 +261,21 @@ export const News = (props) => {
       pageSize: size,
       country: country,
       apiKey: key,
-    }).then((data) => setNews(data));
+    }).then((data) => {
+      // console.log(data);
+      setNews(data);
+    });
     setCurEndpoint(everything);
   };
 
   useEffect(() => {
     if (!isLoaded) {
-      const size = 10;
-      const country = "ru";
       const promis = getNews({
         endpoint: curEndpoint,
         category: curCategory,
         domains: allSources,
+        from: fromDate,
+        to: toDate,
         page: page,
         pageSize: size,
         country: country,
@@ -241,7 +300,7 @@ export const News = (props) => {
       {show && (
         <DateTimePicker
           testID="dateTimePicker"
-          value={date}
+          value={curDate === "from" ? fromDate : toDate}
           mode={mode}
           is24Hour={true}
           display="default"
@@ -268,12 +327,16 @@ export const News = (props) => {
             style={styles.searchInput}
           />
           <Button
-            onPress={showDatepicker}
-            title={`от ${fromDate.getDate()}-${fromDate.getMonth()}`}
+            onPress={showDatepicker.bind(null, "from")}
+            title={`от ${fromDate.toLocaleDateString().split("/")[1]}-${
+              fromDate.toLocaleDateString().split("/")[0]
+            }`}
           />
           <Button
-            onPress={showDatepicker}
-            title={`до ${toDate.getDate()}-${toDate.getMonth()}`}
+            onPress={showDatepicker.bind(null, "to")}
+            title={`до ${toDate.toLocaleDateString().split("/")[1]}-${
+              toDate.toLocaleDateString().split("/")[0]
+            }`}
           />
           <Button onPress={searching} title={"поиск"} />
         </View>
@@ -354,11 +417,13 @@ const styles = StyleSheet.create({
   searchForm: {
     flexDirection: "row",
     justifyContent: "space-between",
-    borderWidth: 1,
-    borderColor: "#000",
   },
   searchInput: {
     width: "50%",
+
+    borderWidth: 1,
+    borderColor: "#c4c4c4ff",
+    borderRadius: 5,
   },
   categories: {
     justifyContent: "space-between",
